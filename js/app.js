a60df9f4ab5b6814ce8764ddb3eaad4715c8da6e
Erin Hinkle <erinhinx5@gmail.com>
2014-09-08 16:25:23 -0400

    change

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
-	$(".button-text").on("click",function (){
-	var item = $(".add-textbox").val();
-	console.log(item)	
- 	});
+	$(".button-text").on("click", additem);
 	$(".add-textbox").on("keypress",function(event){
 	if (event.which == 13) {
+		additem()
+}
+});
+	function additem() {
 	var item = $(".add-textbox").val();
-	console.log(item)			
-	} 	
+	console.log(item)	
+}
 });
-});
+
+
