3f79bc6d8aeab7fa6f6af8ed3e9371b2c731f2c9
Erin Hinkle <erinhinx5@gmail.com>
2014-09-15 15:34:54 -0400

    g

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-	$(".lower-page").hide();
-	$("li").hide();
-	$(".check-button").hide();
-	$(".delete-button").hide();
+	//$(".lower-page").hide();
+	//$("li").hide();
+	//$(".check-button").hide();
+	//$(".delete-button").hide();
 	$(".button-text").on("click", additem);
 	$(".add-textbox").on("keypress",function(event){
 	if (event.which == 13) {
@@ -11,20 +11,30 @@ $(document).ready(function() {
 });
 	function additem() {
 	var item = $(".add-textbox").val();
+	var clone1 = '<li>';
+	var clone2 = '<div class="check-button" style="display: none;">
+							<a href="#">
+								<img class="item-buttons" src="images/blue-check.png">
+							</a>
+						</div>
+						<div class="delete-button" id=" item-buttons""="" style="display: none;">
+						</div>
+					</li>';
 	console.log(item);
-	$(".lower-page").show();
-	$("li").show();	
-	$("li").append(item);
-	$(".check-button").show();
-	$(".delete-button").show();
+	//$(".lower-page").show();
+	//$("li").show();	
+	$("ol").append(clone1, item, clone2);
+	//$(".check-button").show();
+	//$(".delete-button").show();
 }
 	function addanother() {
 	var item = $(".add-textbox").val();
 	console.log(item);
 	$("li").append("<li>"+ item + "</li>");
-	$(".check-button").show();
-	$(".delete-button").show();
+	//$(".check-button").show();
+	//$(".delete-button").show();
 	}
 
 });
+
 
