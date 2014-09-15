5654c8a5abdb1c4400492c98a5952c61de7154fa
Erin Hinkle <erinhinx5@gmail.com>
2014-09-15 16:14:14 -0400

    f

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
-	//$(".lower-page").hide();
-	//$("li").hide();
-	//$(".check-button").hide();
-	//$(".delete-button").hide();
+	$(".lower-page").hide();
+	$("li").hide();
 	$(".button-text").on("click", additem);
 	$(".add-textbox").on("keypress",function(event){
 	if (event.which == 13) {
@@ -14,19 +12,12 @@ $(document).ready(function() {
 	var clone1 = '<li>';
 	var clone2 = '<div class="check-button"><a href="#"><img class="item-buttons" src="images/blue-check.png"/></a></div><div class="delete-button"><a href="#"><img class="item-buttons" src="images/x.png"/></a></div>';
 	console.log(item);
-	//$(".lower-page").show();
-	//$("li").show();	
+	$(".lower-page").show();
+	$("li").show();	
 	$("ol").append(clone1+item+clone2);
-	//$(".check-button").show();
-	//$(".delete-button").show();
 	$(".add-textbox").val("");
 }
-	function addanother() {
-	var item = $(".add-textbox").val();
-	console.log(item);
-	$("li").append("<li>"+ item + "</li>");
 
-	}
 });
 
 
