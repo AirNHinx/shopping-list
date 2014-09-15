05297d93df4c54a033f6076c6bb916decf00998c
Erin Hinkle <erinhinx5@gmail.com>
2014-09-15 15:55:31 -0400

    h

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 	function additem() {
 	var item = $(".add-textbox").val();
 	var clone1 = '<li>';
-	var clone2 = '<div class="check-button";"><a href="#"><img class="item-buttons" src="images/blue-check.png"></a></div><div class="delete-button" id=" item-buttons""="";"></div></li>';
+	var clone2 = '<div class="check-button"><a href="#"><img class="item-buttons" src="images/blue-check.png"/></a></div><div class="delete-button"><a href="#"><img class="item-buttons" src="images/x.png"/></a></div>';
 	console.log(item);
 	//$(".lower-page").show();
 	//$("li").show();	
