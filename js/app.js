b9e3e99f8713ce2ea0640ceb93715244b4cdfa45
Erin Hinkle <erinhinx5@gmail.com>
2014-09-16 12:15:46 -0400

    l

--- js/app.js
+++ js/app.js
@@ -24,12 +24,11 @@ $(document).ready(function() {
    console.log(t.hasClass("action-complete"));
    if(t.hasClass("action-complete")) {
    		console.log("action-complete");
-   	$("li").addClass("strikethrough");	
+   	$(".action-complete").parents().addClass("strikethrough");	
    } else if(t.hasClass("action-delete")) {
    		console.log("action-delete");
    		/*$(".action-delete").parents().remove();
    		$(".lower-page").hide();*/
-   		console.log(t).parents("action-complete");	
    		/*remove the li from the DOM (.remove)*/
    		/*use .parents to find the nearest li so we can act on them*/
 
