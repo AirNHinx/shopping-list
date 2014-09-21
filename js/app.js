d77284b3e7d96bba7a3b203503593393f8530242
Erin Hinkle <erinhinx5@gmail.com>
2014-09-21 19:13:34 -0400

    ,

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
-	/*$(".lower-page").hide();*/
+	$(".lower-page").hide();
 	$(".button-text").on("click", additem);
 	$(".add-textbox").on("keypress",function(event){
-	if (event.which = 13) {
+	if ( event.which == 13 ) {
 		additem();		
 }
 });	
 
-	function additem() {
-	/*$(".lower-page").show();*/	
+	function additem() {	
 	var item = $(".add-textbox").val();
 	var clone1 = '<li>';
 	var clone2 = '<div class="check-button action-complete"><a href="#">';
 	var clone3 = '<img class="item-buttons action-complete" src="images/blue-check.png"/></a></div>';
 	var clone4 = '<div class="delete-button action-delete"><a href="#"><img class="item-buttons action-delete" src="images/x.png"/></a></div>';
 	console.log(item);
+	$(".lower-page").show();
 	$("ol").append(clone1+item+clone2+clone3+clone4);
 	$(".add-textbox").val("");
 }
@@ -25,12 +25,11 @@ $(document).ready(function() {
    console.log(t.hasClass("action-complete"));
    if(t.hasClass("action-complete")) {
    		console.log("action-complete");
+   		/*console.log(this);*/
    	$(t).closest("li").toggleClass("strikethrough");	
    } else if(t.hasClass("action-delete")) {
    		console.log("action-delete");
    		$(t).closest("li").remove();
-   		/*remove the li from the DOM (.remove)*/
-   		/*use .parents to find the nearest li so we can act on them*/
    }
 });
 
