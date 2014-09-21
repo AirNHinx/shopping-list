757a22a20dedeac78764c0fdd08cd248ce60edc5
Erin Hinkle <erinhinx5@gmail.com>
2014-09-21 18:28:48 -0400

    nope

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
-	var listbox = $(".lower-page").show();
 	$(".lower-page").hide();
 	$("li").hide();
-	$(".button-text").on("click", listbox, additem);
+	$(".button-text").on("click", additem);
 	$(".add-textbox").on("keypress",function(event){
 	if (event.which == 13) {
-		$(listbox);
+		$(".lower-page").show();
 		additem();		
 }
 });
