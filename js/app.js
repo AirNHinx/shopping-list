a2ad995e36d2694ee609db9a951ca7178e745f8e
Erin Hinkle <erinhinx5@gmail.com>
2014-09-21 17:51:10 -0400

    nbnm

--- js/app.js
+++ js/app.js
@@ -26,7 +26,7 @@ $(document).ready(function() {
    console.log(t.hasClass("action-complete"));
    if(t.hasClass("action-complete")) {
    		console.log("action-complete");
-   	$(this).closest().parents().toggleClass("strikethrough");	
+   	$(this).parents().closest().toggleClass("strikethrough");	
    } else if(t.hasClass("action-delete")) {
    		console.log("action-delete");
    		/*$(".action-delete").parents().remove();
