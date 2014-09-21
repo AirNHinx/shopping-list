ce9469dac8fd2cdad49cf0a8600cc3c51c7df711
Erin Hinkle <erinhinx5@gmail.com>
2014-09-21 19:15:02 -0400

    jhk

--- js/app.js
+++ js/app.js
@@ -14,8 +14,8 @@ $(document).ready(function() {
 	var clone3 = '<img class="item-buttons action-complete" src="images/blue-check.png"/></a></div>';
 	var clone4 = '<div class="delete-button action-delete"><a href="#"><img class="item-buttons action-delete" src="images/x.png"/></a></div>';
 	console.log(item);
-	$(".lower-page").show();
 	$("ol").append(clone1+item+clone2+clone3+clone4);
+	$(".lower-page").show();
 	$(".add-textbox").val("");
 }
 
