6d8ade30295cf937b8059d0a1ba1d0779d7e2eb7
Erin Hinkle <erinhinx5@gmail.com>
2014-09-21 18:31:52 -0400

    enter

--- js/app.js
+++ js/app.js
@@ -1,12 +1,11 @@
 $(document).ready(function() {
 	$(".lower-page").hide();
 	$("li").hide();
-	$(".button-text").on("click", additem);
 	$(".add-textbox").on("keypress",function(event){
-	if (event.which == 13) {
-		$(".lower-page").show();
-		additem();		
-}
+		if (event.which == 13) {
+			additem();
+	}
+	$(".button-text").on("click", additem);
 });
 	function additem() {
 	var item = $(".add-textbox").val();
