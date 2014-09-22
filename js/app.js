869842fbc8707232b168067d1edeb0bffd6293cf
Erin Hinkle <erinhinx5@gmail.com>
2014-09-21 22:23:26 -0400

    k

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
 	/*$(".lower-page").hide();*/
+	$(".button-text").on("click", additem);
+	$(".add-textbox").on("keypress", function(event){
+	if( event.which == 13 ) {
+		additem();		
+}
+});
+
+	function additem() {
+	var item = $(".add-textbox").val();
+	var clone1 = '<li>';
+	var clone2 = '<div class="check-button action-complete"><a href="#">';
+	var clone3 = '<img class="item-buttons action-complete" src="images/blue-check.png"/></a></div>';
+	var clone4 = '<div class="delete-button action-delete"><a href="#"><img class="item-buttons action-delete" src="images/x.png"/></a></div>';
+	/*console.log(item);*/
+	$(".lower-page").show();
+	$("ol").append(clone1+item+clone2+clone3+clone4);
+	$(".add-textbox").val("");
+}
 
    $("ol.items").on("click", function(event) {
    var t = $(event.target);
@@ -14,25 +32,6 @@ $(document).ready(function() {
    		$(t).closest("li").remove();
    }
 });
-
-	$(".button-text").on("click", additem);
-	$(".add-textbox").on("keypress", function(event){
-	if( event.which == 13 ) {
-		additem();		
-}
-});	
-
-	function additem() {
-	var item = $(".add-textbox").val();
-	var clone1 = '<li>';
-	var clone2 = '<div class="check-button action-complete"><a href="#">';
-	var clone3 = '<img class="item-buttons action-complete" src="images/blue-check.png"/></a></div>';
-	var clone4 = '<div class="delete-button action-delete"><a href="#"><img class="item-buttons action-delete" src="images/x.png"/></a></div>';
-	/*console.log(item);*/
-	$(".lower-page").show();
-	$("ol").append(clone1+item+clone2+clone3+clone4);
-	$(".add-textbox").val("");
-}
 
 });
 
